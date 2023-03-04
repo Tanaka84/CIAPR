6f28b825629611030fab5388aa41102fe23eaa10
Papamore <nickolas.gonzalez1@gmail.com>
2023-03-04 11:37:30 -0400

    agregar par-impar

---
+++ Recursos/Scripts Ejemplo/Par impar.R
@@ -0,0 +1,12 @@
+parimp <- (#aqui va numero pensado?){
+  while (#intentos < dificultad/2) {
+    if(#numero pensado %% 2) == 0) {
+      Return (print ("Es par"))
+      break
+      }
+else if (#numero pensado %% 2) != 0){
+  Return (print ("Es inmpar"))
+  break
+  }
+}
+}
